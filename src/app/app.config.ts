6d28e5f727be72bd025f44d169711334eceb820c
Carlos Gomes <carlos_gomes1809@hotmail.com>
2024-06-24 16:32:49 -0300

    set up routes and some config in app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withPreloading,
+} from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(
+      routes,
+      withComponentInputBinding(),
+      withPreloading(PreloadAllModules)
+    ), // withComponentInputBinding is a function that uses the component's input bindings to provide the route params
+    // withPreloading is a function that preloads all the modules in the routes (prefetching the modules' code and resources)
+    provideHttpClient(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from '@info/pages/not-found/not-found.component';
+import { LayoutComponent } from '@shared/components/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    // nested routes, shared layout by all children
+    children: [
+      {
+        path: '',
+        // lazy loading the list component
+        loadComponent: () =>
+          import('@products/pages/list/list.component').then(
+            (m) => m.ListComponent
+          ),
+      },
+      {
+        path: 'about',
+        loadComponent: () =>
+          import('@info/pages/about/about.component').then(
+            (m) => m.AboutComponent
+          ),
+      },
+      {
+        path: 'product/:id',
+        loadComponent: () =>
+          import(
+            '@products/pages/product-detail/product-detail.component'
+          ).then((m) => m.ProductDetailComponent),
+      },
+    ],
+  },
+  // This route will be used when the user navigates to a non-existing route, it should be the last one as it will match any route
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
+];
